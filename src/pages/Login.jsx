e5996316261348b872f85df595f5a95ce78ebec0
gabrielalberini <gabialberini733@gmail.com>
2025-11-20 18:00:45 -0300

    inclusión de query params en la ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom"
 import Layout from "../components/Layout"
+import { useAuth } from "../context/AuthContext"
 
 const Login = () => {
+  const { login } = useAuth()
+  const navigateUser = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    login()
+    navigateUser("/")
+  }
+
   return (
     <Layout>
       <div className="center-auth">
-        <form className="form-container">
+        <form className="form-container" onSubmit={handleSubmit}>
           <h3>Iniciar Sesión</h3>
           <input type="email" placeholder="Email" required />
           <input type="password" placeholder="Contraseña" required />
